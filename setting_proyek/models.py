06722e7610de0c551a1667841e70e182eb11ec9d
Your Name <you@example.com>
2016-01-23 11:09:27 +0700

    update add user di setting proyek

--- setting_proyek/models.py
+++ setting_proyek/models.py
@@ -1,14 +1,18 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
 class Proyek(models.Model):
 	nomor_proyek =	models.CharField(max_length=10,unique=True)
 	nama_proyek = models.CharField(max_length=200, blank=False)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp =  models.DateTimeField(auto_now=False, auto_now_add=True)
-	pengguna = models.CharField(max_length=4, choices= ((u'H', u'High',), (u'M', u'Medium'), (u'L', u'Low')))
+	pengguna = models.ForeignKey(User)
+
+	# pengguna = models.CharField(max_length=4, choices= ((u'H', u'High',), (u'M', u'Medium'), (u'L', u'Low')))
 
 	def __unicode__(self):
 		return self.nomor_proyek
